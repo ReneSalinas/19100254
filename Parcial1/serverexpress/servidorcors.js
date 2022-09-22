85dd9e216398c31ddb8b56e5d674e2a509e716a2
ReneSalinas <renesalinas0525@gmail.com>
2022-09-21 21:17:04 -0500

    html

--- Parcial1/serverexpress/servidorcors.js
+++ Parcial1/serverexpress/servidorcors.js
@@ -1,18 +1,26 @@
 const express = require('express')
 const cors = require('cors')
+const res = require('express/lib/response')
 
 const app = express()
 
 app.use(cors({ origin:"http://127.0.0.1:5500"}))
 
 app.get('/', (req, res) => {
-    res.send('Servidor Express contestando a get puerto 8082')
-})
+    //res.send('Servidor Express contestando a get puerto 8082')
+    res.sendFile('./static/index.html',{root:__dirname})
+})              
 
 app.post('/', (req, res) => {
-    res.send('Servidor Express contestando a post puerto 8082')
+    // res.send('Servidor Express contestando a post puerto 8082')
+    res.json({usuario:"RENE"})
+    res.send('HOLAAAAAAAAAAAAAAAAAAAAAAAAA')
 })
 
-app.listen(8082, () =>{
-    console.log('Servidor express escuchando en pto 8082')
+app.use(function (req,res){
+    res.status(404).sendFile("/static/404.html",{root:__dirname})
+})
+
+app.listen(8085, () =>{
+    console.log('Servidor express escuchando en pto 8085')
 })
