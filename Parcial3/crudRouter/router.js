1fa9bfd7e95de27af42a788a02f57a901191912d
ReneSalinas <renesalinas0525@gmail.com>
2022-11-17 07:29:36 -0600

    router

---
+++ Parcial3/crudRouter/router.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const {query, json} = require('express')
+let json2xls=require('json2xls')
+
+const ruta_empleado=require('./ruta_empleado')
+
+const app = express()
+app.use(express.text())
+app.use(express.json())
+app.use(cors({origin:"http:localhost"}))
+
+
+app.use('/empleado',ruta_empleado.router)
+
+
+
+app.listen(8085, () =>{
+    console.log('Servidor express escuchando en pto 8085')
+})
---
+++ Parcial3/crudRouter/ruta_empleado.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const cors = require('cors')
+var router = express.Router()
+
+const app = express()
+let pg = require('pg')
+
+app.use(cors({origin:"*"}))
+app.use(express.text())
+app.use(express.json())
+
+
+const conString = {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'basechida',
+    password: 'admin',
+    port: '5432'
+  }
+  
+  var pgClient = new pg.Client(conString);
+  pgClient.connect()  
+  
+
+
+
+router.get('/',function(req,res) {
+    
+    if(req.body.text!=undefined){
+        
+        pgClient.query('SELECT * FROM empleado WHERE id =' + req.body)
+        .then(response => {
+        console.log(response.rows)
+        res.send(response.rows)
+    })
+
+    }
+    else{
+        
+    pgClient.query('SELECT * FROM empleado')
+        .then(response => {
+        console.log(response.rows)
+        res.send(response.rows)
+         })
+    }
+
+    
+})
+// POST
+.post('/',function(req,res) {
+    
+    nombre = req.body.nombre
+    apellido = req.body.apellido
+    pgClient.query(`INSERT INTO empleado(nombre,apellido) VALUES ('${nombre}','${apellido}')`)
+    .then(response => {
+        res.send(response)
+    })
+    
+})
+// DELETE
+.delete('/',function(req,res) {
+    
+    let id = req.body.id
+    pgClient.query(`DELETE FROM empleado WHERE id ='${id}'`)
+    .then(response => {
+        res.send(`Empleado eliminado`)
+    })
+    
+})
+// PUT PATCH
+.patch('/',function(req,res) {
+    
+    let id = req.body.id
+    let nombre = req.body.nombre
+    let apellido = req.body.apellido
+    pgClient.query(`UPDATE empleado SET nombre='${nombre}',apellido='${apellido}'  WHERE id='${id}'`)
+    .then(response => {
+        res.send(`Empleado actualizado`)
+    })
+    
+})
+
+// app.listen(8085, () =>{
+//     console.log('Servidor express escuchando en pto 8085')
+// })
+module.exports.router=router
