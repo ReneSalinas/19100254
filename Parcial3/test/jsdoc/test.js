bdc1dda655e0a992d61d2299b09e4d7c46a2acd3
ReneSalinas <renesalinas0525@gmail.com>
2022-11-24 12:16:31 -0600

    ya quedo el test

--- Parcial3/test/jsdoc/test.js
+++ Parcial3/test/jsdoc/test.js
@@ -2,12 +2,12 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 const expect   = require('chai').expect;
 chai.use(chaiHttp);
-const url= 'http://localhost:8082';
+const url= 'http://localhost:8085';
 // Encapsular en test dentro de la funcion describe() Y describirmos el test
 describe('Inserta un empleado: ',()=>{
     it('deberia insertar un empleado', (done) => {// En la funcion it() lo que deberia hacer
         chai.request(url)
-            .post('/empleado')
+            .post('/')
             .send({ nombre:"Enrique", apellido:"Pena" })
             .end( function(err,res){
                 expect(res).to.have.status(200);
@@ -21,7 +21,7 @@ describe('Inserta un empleado: ',()=>{
 describe('Obtiene empleados: ',()=>{
     it('Deberia obtener todos los empleados', (done) => {
         chai.request(url)
-                .get('/empleado')
+                .get('/')
                 .send()
                 .end( function(err,res){
                     expect(res).to.have.status(200);
