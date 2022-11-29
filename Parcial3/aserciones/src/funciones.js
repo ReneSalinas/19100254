291a444db0ae01c049ce50c7a31bd3403209118c
ReneSalinas <renesalinas0525@gmail.com>
2022-11-29 08:33:20 -0600

    test chai

---
+++ Parcial3/aserciones/src/funciones.js
@@ -0,0 +1,7 @@
+const potencia = (numerobase,exponente) => {
+    return Math.pow(numerobase,exponente);
+}
+
+// console.log(potencia(2,3));
+
+exports.potencia = potencia
---
+++ Parcial3/aserciones/test/test.js
@@ -0,0 +1,32 @@
+let funciones = require('../src/funciones.js');
+let chai = require('chai');
+let shoul = chai.should();
+let expect = chai.expect;
+let assert = chai.assert;
+
+describe('Pruebas de la funcion potencia', function(){
+    it('Deberia regresar un 8',function(){
+        let resultado = funciones.potencia(2,3);
+        resultado.should.be.a('number');
+        resultado.should.equal(8);
+    })
+})
+
+describe('Pruebas de la funcion potencia', function(){
+    it('Deberia regresar un 8',function(){
+        let resultado = funciones.potencia(2,3);
+        assert.typeOf(resultado,'number');
+        assert.equal(resultado,8);
+    })
+})
+
+describe('Pruebas de la funcion potencia', function(){
+    it('Deberia regresar un 8',function(){
+        let resultado = funciones.potencia(2,3);
+        expect(resultado).to.be.a('number');
+        expect(resultado).to.equal(8);
+    })
+})
+
+
+console.log(funciones.potencia(2,3));
