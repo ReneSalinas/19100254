ce0f6fa7300e7f4a14436f6b1d7c1fd5abd520b5
ReneSalinas <renesalinas0525@gmail.com>
2022-12-04 12:08:30 -0600

    swagger

--- Api/server.js
+++ Api/server.js
@@ -16,6 +16,16 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'templates/index.html'));
 });
 
+/**
+* @swagger
+* /premio:
+*   get:
+*     description: Realizado para consultar un premio al azar
+*     responses:
+*       200:
+*         description: Permite consultar y traer todos sus datos de un premio o producto al azar.
+*/
+
 app.get('/premio',async function(req, res) {
   let tam = await tamano()
   console.log(tam[0].idproducto)
@@ -25,22 +35,57 @@ app.get('/premio',async function(req, res) {
   res.json(premio[0])
 });
 
-
+/**
+ * @swagger
+ * /listadepremios:
+ *  get:
+ *    descripcion: Consulta toda la base de datos completa de premios.
+ *    responses:
+ *      200:
+ *        description: Se mostrara todos los elementos de la base de datos.
+ */
 app.get('/listadepremios',async function(req, res){
   premios = await getlistadepremios()
   res.json(premios)
 })
 
+/**
+ * @swagger
+ * /agregaPremio:
+ *  post:
+ *    descripcion: Consulta para poder agregar un producto o premio mas a la base de datos.
+ *    responses:
+ *      200:  Publicado exitoso
+ *        description: Se agregara un premio con los datos especificados en el body.
+ */
 app.post('/agregaPremio', async function (req, res) {
   premio = await agregaPremio(req.body.nombre,req.body.descripcion,req.body.urlpremio)
   res.send(premio)
 })
 
+/**
+ * @swagger
+ * /eliminar:
+ * delete:
+ *    descripcion: Consulta para eliminar el producto o premio con el idproducto especificado.
+ *    responses:
+ *      200:  PRODUCTO eliminado.
+ *        description: Se eliminara el premio o producto de la base de datos.
+ */
 app.delete('/eliminar', async function (req,res) {
   borrado = await quitaPremio(req.body.idproducto)
   res.send(borrado)
 })
 
+/**
+ * @swagger
+ * /editar:
+ * patch:
+ *    descripcion: Consulta para actualizar o editar algun dato de un producto especificado con su idproducto.
+ *    responses:
+ *      200:  Producto Actualizado.
+ *        description: Se actualizara el registro con la nueva implementacion o cambio de datos.
+ */
 app.patch('/editar', async function (req, res) {
   editado = await actualizaPremio(req.body.idproducto,req.body.nombre,req.body.descripcion,req.body.urlpremio)
   res.send(editado)
